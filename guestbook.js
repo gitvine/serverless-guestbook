ecb03be533e8089a4f692f9273b8d010c8b339cd
gitvine <74828961+gitvine@users.noreply.github.com>
2022-02-08 21:42:39 -0500

    Update guestbook.js
    changed api

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://976a3d07.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
